b9014e3b3ac22671ccb9309c92db0955d306b04a
RyanOfori <ryanofori1@gmail.com>
2023-02-14 23:13:46 -0500

    Add topAppBar

--- app/src/main/java/com/ryanofori/watchlist/MainActivity.kt
+++ app/src/main/java/com/ryanofori/watchlist/MainActivity.kt
@@ -3,11 +3,14 @@ package com.ryanofori.watchlist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ryanofori.watchlist.ui.theme.WatchlistTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +32,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScaffold() {
-    Text("Hello")
+    Column {
+        TopAppBar(
+            elevation = 4.dp,
+            title = {Text(stringResource(id = R.string.app_name))},
+            backgroundColor = MaterialTheme.colors.primary
+        )
+        Text("Hello")
+    }
+
 }
 
 
